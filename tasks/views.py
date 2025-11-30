00bfa7b84a779ff2ba85fb759c91f6a60622c5d1
Nrnove <nurnove2005@gmail.com>
2025-12-01 00:20:16 +0600

    Updated Tailwind, settings, views and urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Home View
-def home_view(request):
-    return render(request,"dashboard.html")
-
-# Contact View
-def contact_view(request):
-    return HttpResponse("This is the contact page. Reach us at contact@taskmanagement.com")
-
 # Create your views here.
+
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
